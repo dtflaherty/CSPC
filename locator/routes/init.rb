31194d8892cc14a9b977099065226f4563fd0276
Wayne Graham <wayne.graham@virginia.edu>
2012-12-06 15:20:25 -0500

    an initial map

--- locator/routes/init.rb
+++ locator/routes/init.rb
@@ -1,5 +1,12 @@
+require 'geocoder'
+
 class Locator < Sinatra::Base
   get '/' do
     erb :index
+  end
+
+  get '/map/:location' do
+    @locations = Geocoder.search(params[:location])
+    erb :map
   end
 end
