ea029b75159cc3805fb6dd02513eaa1bc268f4a5
Wayne Graham <wayne.graham@virginia.edu>
2013-04-22 12:32:25 -0400

    Changes bad data to cdata
    Updates the add_docs to enclose cdata in the values
    updates the post script with a note about the sed used to change the cdata

--- project_files/post.sh
+++ project_files/post.sh
@@ -3,6 +3,10 @@
 if [ "$#" -eq 0 ]; then
   echo "USAGE: ./post.sh server_ip"
 else
+  #TODO sed to replace 
+  # s/&lt;![CDATA[/<![CDATA[/g
+  # s/]]&gt;</field>/]]></field>/g
+
   curl http://$1:8080/solr/update?commit=true  -H "Content-Type: text/xml" --data-binary @add_doc.xml
 fi
 
