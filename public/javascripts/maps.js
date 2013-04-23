1852c151b8212405b23a4041cd56e19c3681c270
Wayne Graham <wayne.graham@virginia.edu>
2013-04-23 12:52:47 -0400

    refactor map in to it's own JS file, reorders the navigation orger

---
+++ public/javascripts/maps.js
@@ -0,0 +1,40 @@
+$(function() {
+  OpenLayers.IMAGE_RELOAD_ATTEMPTS = 3;
+  OpenLayers.ImgPath = 'http://js.mapbox.com/theme/dark/';
+
+  var SHADOW_Z_INDEX = 10;
+  var MARKER_Z_INDEX = 11;
+  var DIAMETER = 200;
+
+  var map, layer, center, marker;
+  var proj = new OpenLayers.Projection("EPSG:4326");
+  var center = new OpenLayers.LonLat(-8795761.71761, 3404810.9874);
+
+
+  var options = {
+    controls: [
+      new OpenLayers.Control.Navigation(),
+      new OpenLayers.Control.PanZoomBar(),
+      new OpenLayers.Control.LayerSwitcher({'ascending':false}),
+      new OpenLayers.Control.Permalink(),
+      new OpenLayers.Control.MousePosition(),
+      new OpenLayers.Control.KeyboardDefaults(),
+      new OpenLayers.Control.LayerSwitcher()
+    ]
+  };
+
+  map = new OpenLayers.Map('map', options);
+
+  var gphy = new OpenLayers.Layer.Google(
+    "Google Physical",
+    {type: google.maps.MapTypeId.TERRAIN}
+  );
+
+  map.addLayers([gphy]);
+
+
+  if(!map.getCenter()) {
+    map.setCenter(center, 3);
+  }
+
+});
