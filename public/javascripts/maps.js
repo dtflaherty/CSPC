11a1a38e1fa097bbd7ccca7d2a888dce82f8a833
Wayne Graham <wayne.graham@virginia.edu>
2013-04-23 13:20:25 -0400

    updates to the map

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -6,7 +6,13 @@ $(function() {
   var MARKER_Z_INDEX = 11;
   var DIAMETER = 200;
 
-  var map, layer, center, marker;
+  // marker settings
+  var size = new OpenLayers.Size(21,25);
+  var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+  var icon = new OpenLayers.Icon('/images/marker-gold.png',size,offset);
+
+
+  var map, layer, markers;
   var proj = new OpenLayers.Projection("EPSG:4326");
   var center = new OpenLayers.LonLat(-8795761.71761, 3404810.9874);
 
@@ -32,6 +38,10 @@ $(function() {
 
   map.addLayers([gphy]);
 
+  markers = new OpenLayers.Layer.Markers("Markers");
+  map.addLayers(markers);
+
+  
 
   if(!map.getCenter()) {
     map.setCenter(center, 3);
