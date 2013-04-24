feb746700c0f97a9ffb9e4215fa37f202c804369
Wayne Graham <wayne.graham@virginia.edu>
2013-04-23 21:32:32 -0400

    updates the projection

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -5,7 +5,7 @@ $(function() {
   //var google = google || {};
 
   OpenLayers.IMAGE_RELOAD_ATTEMPTS = 3;
-  OpenLayers.ImgPath = 'http://js.mapbox.com/theme/dark/';
+  //OpenLayers.ImgPath = 'http://js.mapbox.com/theme/dark/';
 
   var SHADOW_Z_INDEX = 10;
   var MARKER_Z_INDEX = 11;
@@ -14,14 +14,16 @@ $(function() {
   // marker settings
   var size = new OpenLayers.Size(21,25);
   var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
-  var icon = new OpenLayers.Icon('/images/marker-gold.png',size,offset);
-
+  //var icon = new OpenLayers.Icon('/images/marker-gold.png',size,offset);
+  var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png',size,offset);
 
   var map, layer, markers, places;
   var proj = new OpenLayers.Projection("EPSG:4326");
   var center = new OpenLayers.LonLat(-5800000, 4100000);
 
   var options = {
+    projection: new OpenLayers.Projection("EPSG:900913"),
+    displayProjection: new OpenLayers.Projection("EPSG:4326"),
     controls: [
       new OpenLayers.Control.Navigation(),
       new OpenLayers.Control.PanZoomBar(),
@@ -37,12 +39,31 @@ $(function() {
 
   var gphy = new OpenLayers.Layer.Google(
     "Google Satellite",
-    {type: google.maps.MapTypeId.SATELLITE}
+    {
+      type: google.maps.MapTypeId.SATELLITE,
+      'sphericalMercator': true
+    }
   );
 
-  places = new OpenLayers.Layer.Text( "text", { location: "/fixed_points.txt" } );
+  //places = new OpenLayers.Layer.Text(
+  //"text", {
+  //location: "/fixed_points.txt"
+  //}
+  //);
 
-  console.log(places);
+  places = new OpenLayers.Layer.Vector("TextLayer", {
+    projection: proj,
+    strategies: [new OpenLayers.Strategy.Fixed()],
+    protocol: new OpenLayers.Protocol.HTTP({
+      url: "/fixed_points.txt",
+      format: new OpenLayers.Format.Text({
+        extractStyles: true,
+        extractAttributes: true
+      })
+    })
+  });
+
+  console.log(map.displayProjection);
 
   markers = new OpenLayers.Layer.Markers("Markers");
 
